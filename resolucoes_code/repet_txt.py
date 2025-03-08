446f5f1301d6e861941d2c61cc908f61969aeb36
vaurley <vaurley4@gmail.com>
2025-03-08 16:37:19 -0300

    código para repetição de texto

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,6 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Vamos solicitar como entrada uma string e um numero inteiro e repetir.
+
+string_input = input("Digite uma string: ")
+integer_input = int(input("Digite um número inteiro: "))
+
+print((string_input + " ") * integer_input )
